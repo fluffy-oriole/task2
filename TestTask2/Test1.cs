a7c18cc406170d2826e9c9c05c5acb13cf85a564
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-02-20 01:05:40 +0800

    добавленны тесты

---
+++ TestTask2/Test1.cs
@@ -0,0 +1,56 @@
+﻿using task2;
+
+namespace TestTask2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void NoSubsequence()
+        {
+            int[] sequence = new int[] { 1, 2, 3, 4, 5 };
+            int answer = Logic.FindSequenceNumber(sequence, 5, 5);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [TestMethod]
+        public void NormalSubsequence()
+        {
+            int[] sequence = new int[] { 1, 2, 2, 2, 3 };
+            int answer = Logic.FindSequenceNumber(sequence, 5, 3);
+            Assert.AreEqual(1, answer);
+        }
+        
+        [TestMethod]
+        public void EmptySequence()
+        {
+            int[] sequence = new int[] { };
+            int answer = Logic.FindSequenceNumber(sequence, 0, 3);
+            Assert.AreEqual(-1, answer);
+        }
+        
+        [TestMethod]
+        public void EmptySubsequenceLength()
+        {
+            int[] sequence = new int[] { 1, 2, 2, 2, 3 };
+            int answer = Logic.FindSequenceNumber(sequence, 5, 0);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [TestMethod]
+        public void SubsequenceAtTheEnd()
+        {
+            int[] sequence = new int[] { 1, 2, 3, 4, 4 };
+            int answer = Logic.FindSequenceNumber(sequence, 3, 2);
+            Assert.AreEqual(-1, answer);
+        }
+        
+        [TestMethod]
+        public void MoreThanOneSecuence()
+        {
+            int[] sequence = new int[] { 1, 2, 2, 3, 4, 4 };
+            int answer = Logic.FindSequenceNumber(sequence, 6, 2);
+            Assert.AreEqual(1, answer);
+        }
+    }
+}
